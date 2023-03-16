722e501eb92e9f6c12e9192977d08b80ecf88256
LinasKrutkevicius <100715343+LinKru95@users.noreply.github.com>
2023-03-16 19:45:08 +0200

    Added form and validation.

--- Data/Grocery.cs
+++ Data/Grocery.cs
@@ -1,8 +1,16 @@
-﻿namespace BlazorApp.Data
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Data
 {
     public class Grocery
     {
+        [Required]
+        [StringLength(15, ErrorMessage = "Name should be less than 15 characters.")]
         public string Name { get; set; }
+        [Required]
+        [Range(1,1000, ErrorMessage = "Valid price range is 1 to 1000.")]
         public decimal Price { get; set; }
     }
 }
